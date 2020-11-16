9ee095cabdfb882fe760687d43b0fca15e526fc7
sungmin414 <sungmin3231@gmail.com>
2020-11-17 00:00:13 +0900

    javascript navbar menu click

--- PORTFOLIO/js/main.js
+++ PORTFOLIO/js/main.js
@@ -1,14 +1,29 @@
-'use strict';
+"use strict";
 
 // navbar가 상단에있을 때 투명하게 만들기
-const navbar = document.querySelector('#navbar');
+const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-    // console.log(window.scrollY);
-    // console.log(`navbarHeight: ${navbarHeight}`);
-    if(window.scrollY > navbarHeight){
-        navbar.classList.add('navbar--dark');
-    } else {
-        navbar.classList.remove('navbar--dark');
-    }
+document.addEventListener("scroll", () => {
+  // console.log(window.scrollY);
+  // console.log(`navbarHeight: ${navbarHeight}`);
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
+
+// vabar 메뉴를 탭할 때 스크롤 처리
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  // console.log(event.target.dataset.link);
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 });
